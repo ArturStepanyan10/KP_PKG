5e0e2bf0b1803e6a17636c0a1fa97c803322c4f8
ArturStepanyan10 <stepanyanartur110@gmail.com>
2025-03-28 23:23:44 +0300

    Добавление некоторых объектов: тела вращения и дорбаотка некоторых элементов

--- KP_Stepanyan_PRI-121/Form1.Designer.cs
+++ KP_Stepanyan_PRI-121/Form1.Designer.cs
@@ -34,6 +34,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.RenderTimer = new System.Windows.Forms.Timer(this.components);
             this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // AnT
@@ -54,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(756, 57);
+            this.button1.Location = new System.Drawing.Point(702, 45);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 23);
             this.button1.TabIndex = 3;
@@ -64,7 +66,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(756, 142);
+            this.button3.Location = new System.Drawing.Point(702, 122);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(150, 52);
             this.button3.TabIndex = 4;
@@ -79,7 +81,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(756, 101);
+            this.button4.Location = new System.Drawing.Point(702, 84);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(150, 23);
             this.button4.TabIndex = 5;
@@ -87,11 +89,32 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(702, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 58);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Увеличить резкость";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(699, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "----------------------------------------------------------";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(979, 572);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
@@ -99,6 +122,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -109,6 +133,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Timer RenderTimer;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- KP_Stepanyan_PRI-121/Form1.cs
+++ KP_Stepanyan_PRI-121/Form1.cs
@@ -13,8 +13,6 @@ namespace KP_Stepanyan_PRI_121
     {
         private float lionX = -2.0f;
         private float lionZ = 1.5f;
-        
-        private float global_time = 0;
 
         private bool isInsideHouse = false;
         private bool isLightOn = false;
@@ -40,6 +38,12 @@ namespace KP_Stepanyan_PRI_121
         private bool hasEars = false;          // Флаг наличия ушек
         private bool isSmallHeadMode = false;
 
+        private bool isSharpnessEffectActive = false;
+        private float sharpnessIntensity = 0.3f;
+
+        private float vaseRotationAngle = 0.0f; // Угол вращения вазы
+        private float vaseRotationSpeed = 1.0f;
+
         public Form1()
         {
             InitializeComponent();
@@ -54,10 +58,8 @@ namespace KP_Stepanyan_PRI_121
             this.KeyPreview = true; // Разрешаем форме обрабатывать события клавиш
             AnT.TabStop = true;
 
-            
-            
-            RenderTimer.Start();
 
+            RenderTimer.Start();
         }
 
         private void SetupScene()
@@ -68,7 +70,6 @@ namespace KP_Stepanyan_PRI_121
 
         private void RenderScene()
         {
-
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
 
             if (isInsideHouse)
@@ -109,14 +110,17 @@ namespace KP_Stepanyan_PRI_121
                 Gl.glEnd();
 
                 // Деревья
-                DrawTree(-5.0f, 0.0f, -5.0f);
-                DrawTree(3.0f, 0.0f, -3.0f);
-                DrawTree(-8.0f, 0.0f, 1.0f);
-                DrawTree(-5.0f, 0.0f, 6.0f);
-               
-         
-                // Домик
-                DrawHouse(3.0f, 0.0f, 0.0f);
+                if (isSharpnessEffectActive)
+                {
+                    DrawEnhancedHouseAndTrees();
+                }
+                else
+                {
+                    DrawHouse(3.0f, 0.0f, 0.0f);
+                    DrawTree(-5.0f, 0.0f, -5.0f);
+                    DrawTree(-8.0f, 0.0f, 1.0f);
+                    DrawTree(-5.0f, 0.0f, 6.0f);
+                }
 
                 // Лев
                 DrawLion(lionX, -1.9f, lionZ);
@@ -125,12 +129,35 @@ namespace KP_Stepanyan_PRI_121
                 DrawCrane(craneX, craneY, craneZ);
 
                 DrawFractalCloud(-5.0f + cloudOffsetX, 5.0f, -8.0f, 1.5f, 3);
-                DrawFractalCloud(2.0f + cloudOffsetX * 0.8f, 6.0f, -10.0f, 2.0f, 2); // Двигается чуть медленнее
+                DrawFractalCloud(2.0f + cloudOffsetX * 0.8f, 6.0f, -10.0f, 2.0f, 2); 
                 DrawFractalCloud(5.0f + cloudOffsetX * 1.2f, 4.5f, -5.0f, 1.2f, 3);
 
+                
 
                 AnT.Refresh();
             }
+        }
+
+        private void DrawEnhancedHouseAndTrees()
+        {
+            DrawHouse(3.0f, 0.0f, 0.0f);
+            DrawTree(-5.0f, 0.0f, -5.0f);
+            DrawTree(-8.0f, 0.0f, 1.0f);
+            DrawTree(-5.0f, 0.0f, 6.0f);
+
+            Gl.glEnable(Gl.GL_POLYGON_OFFSET_LINE);
+            Gl.glPolygonOffset(-1.0f, -1.0f);
+            Gl.glLineWidth(1.5f);
+            Gl.glColor3f(0.2f, 0.2f, 0.2f);
+            Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_LINE);
+
+            DrawHouse(3.0f, 0.0f, 0.0f);
+            DrawTree(-5.0f, 0.0f, -5.0f);
+            DrawTree(-8.0f, 0.0f, 1.0f);
+            DrawTree(-5.0f, 0.0f, 6.0f);
+
+            Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_FILL);
+            Gl.glDisable(Gl.GL_POLYGON_OFFSET_LINE);
         }
 
         private void SetupLighting()
@@ -160,7 +187,6 @@ namespace KP_Stepanyan_PRI_121
             Glu.gluSphere(body, 1.6f, 16, 16);
             Glu.gluDeleteQuadric(body);
 
-
             // Голова льва
             Gl.glColor3f(0.2f, 0.2f, 0.1f);
             Gl.glPushMatrix();
@@ -168,9 +194,8 @@ namespace KP_Stepanyan_PRI_121
             Glu.GLUquadric head = Glu.gluNewQuadric();
             Glu.gluSphere(head, lionHeadSize, 16, 16);
             
-
             // Рисуем ушки если включены
-            if (hasEars && isInsideHouse)
+            if (hasEars)
             {
                 Gl.glColor3f(0.2f, 0.1f, 0.05f); // Темный цвет ушек
 
@@ -178,26 +203,21 @@ namespace KP_Stepanyan_PRI_121
                 Gl.glPushMatrix();
                 Gl.glTranslatef(-0.5f, 0.8f, 0.3f);
                 Glu.gluSphere(head, 0.3f, 8, 8);
-                
 
                 // Правое ухо
                 Gl.glPushMatrix();
-                Gl.glTranslatef(1.0f, 0.4f, 0.3f);
-                Glu.gluSphere(head, 0.3f, 8, 8);
-                
+                Gl.glTranslatef(1.1f, 0.3f, 0.3f);
+                Glu.gluSphere(head, 0.3f, 8, 8);          
             }
             
             Glu.gluDeleteQuadric(head);
 
-
-
             // Хвост льва
-            Gl.glColor3f(0.5f, 0.3f, 0.1f);
-            
-            Gl.glTranslatef(0.0f, -1.5f, -1.0f);
-            Gl.glRotatef(90, 1.0f, 0.0f, 0.0f);
+            Gl.glColor3f(0.5f, 0.3f, 0.1f);    
+            Gl.glTranslatef(0.0f, -2.5f, -1.0f);
+            Gl.glRotatef(90, 1.0f, 0.0f, 0.5f);
             Glu.GLUquadric tail = Glu.gluNewQuadric();
-            Glu.gluCylinder(tail, 0.3f, 0.3f, 2.0f, 8, 8);
+            Glu.gluCylinder(tail, 0.3f, 0.3f, 2.0f, 6, 6);
             Glu.gluDeleteQuadric(tail);
             Gl.glPopMatrix();
 
@@ -335,12 +355,10 @@ namespace KP_Stepanyan_PRI_121
                 case Keys.NumPad2:
                     newCraneY -= moveStep;
                     break;
-
             }
 
             newLionX = Math.Max(MIN_LION_X, Math.Min(MAX_LION_X, newLionX));
             newLionZ = Math.Max(MIN_LION_Z, Math.Min(MAX_LION_Z, newLionZ));
-
 
             // Проверяем столкновения
             if (!CheckCollision(newLionX, newLionZ))
@@ -355,7 +373,6 @@ namespace KP_Stepanyan_PRI_121
             RenderScene();
         }
 
-
         private void simpleOpenGlControl1_Paint(object sender, PaintEventArgs e)
         {
             RenderScene();
@@ -366,22 +383,16 @@ namespace KP_Stepanyan_PRI_121
             // Границы дома
             float houseX = 3.0f;
             float houseZ = 0.0f;
-            float houseWidth = 4.0f; // Ширина дома
-            float houseDepth = 4.0f; // Глубина дома
+            float houseWidth = 4.0f; 
+            float houseDepth = 4.0f; 
 
             // Проверка столкновения с домом
             if (newX >= houseX - houseWidth / 2 && newX <= houseX + houseWidth / 2 &&
                 newZ >= houseZ - houseDepth / 2 && newZ <= houseZ + houseDepth / 2)
             {
-                return true; // Столкновение с домом
+                return true; 
             }
-
-            return false; // Нет столкновений
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
+            return false; 
         }
 
         // Добавление домика
@@ -400,7 +411,7 @@ namespace KP_Stepanyan_PRI_121
             DrawCube(-1.8f, 0.1f, -1.8f, 3.6f, 1.8f, 3.6f); // Внутренний куб (меньше внешнего)
 
             // Крыша
-            Gl.glColor3f(0.5f, 0.2f, 0.1f); // Цвет крыши
+            Gl.glColor3f(0.5f, 0.2f, 0.1f);
             Gl.glBegin(Gl.GL_TRIANGLES);
             // Передняя часть крыши
             Gl.glVertex3f(-2.0f, 2.0f, 2.0f);
@@ -428,7 +439,7 @@ namespace KP_Stepanyan_PRI_121
             Gl.glEnd();
 
             // Дверь
-            Gl.glColor3f(0.1f, 0.1f, 0.3f); // Черный цвет двери
+            Gl.glColor3f(0.1f, 0.1f, 0.3f);
             Gl.glBegin(Gl.GL_QUADS);
             Gl.glVertex3f(-0.5f, 0.0f, 2.01f);
             Gl.glVertex3f(0.5f, 0.0f, 2.01f);
@@ -484,13 +495,12 @@ namespace KP_Stepanyan_PRI_121
 
         private void button1_Click(object sender, EventArgs e)
         {
-            isInsideHouse = true; // Переключаемся на сцену внутри дома
+            isInsideHouse = true; 
             RenderScene();
         }
 
         private void DrawFractalCloud(float centerX, float centerY, float centerZ, float size, int detail)
         {
-            // Фиксируем Random, чтобы облака не двигались
             Random rand = new Random(GetCloudSeed(centerX, centerY, centerZ));
 
             Gl.glColor3f(1.0f, 1.0f, 1.0f);
@@ -514,21 +524,63 @@ namespace KP_Stepanyan_PRI_121
             Gl.glEnd();
         }
 
-        // Генерирует фиксированный seed для облака на основе его позиции
         private int GetCloudSeed(float x, float y, float z)
         {
             return (int)(x * 100 + y * 10 + z);
         }
 
+        private void DrawRevolvedVase()
+        {
+            // Точки профиля вазы (X - радиус, Y - высота)
+            List<PointF> profilePoints = new List<PointF>
+    {
+        new PointF(0.0f, 0.0f),   // Основание
+        new PointF(0.5f, 0.5f),   // Начало изгиба
+        new PointF(1.2f, 1.5f),   // Самая широкая часть
+        new PointF(0.8f, 2.5f),   // Сужение
+        new PointF(0.4f, 3.0f),   // Горлышко
+        new PointF(0.0f, 3.5f)    // Верх
+    };
+
+            // Интерполяция для гладкости
+            List<PointF> interpolatedPoints = InterpolateLagrange(profilePoints, 30);
+            int numSlices = 36; // Количество сегментов по окружности
+
+            // Цвет вазы (фиолетовый)
+            Gl.glColor3f(0.8f, 0.6f, 0.9f);
+
+            // Рисуем тело вращения
+            for (int i = 0; i < numSlices; i++)
+            {
+                float angle1 = (float)(i * 2.0 * Math.PI / numSlices);
+                float angle2 = (float)((i + 1) * 2.0 * Math.PI / numSlices);
+
+                Gl.glBegin(Gl.GL_QUAD_STRIP);
+                for (int j = 0; j < interpolatedPoints.Count; j++)
+                {
+                    float x = interpolatedPoints[j].X;
+                    float y = interpolatedPoints[j].Y;
+
+                    // Точка 1
+                    float x1 = x * (float)Math.Cos(angle1);
+                    float z1 = x * (float)Math.Sin(angle1);
+                    Gl.glVertex3f(x1, y, z1);
+
+                    // Точка 2
+                    float x2 = x * (float)Math.Cos(angle2);
+                    float z2 = x * (float)Math.Sin(angle2);
+                    Gl.glVertex3f(x2, y, z2);
+                }
+                Gl.glEnd();
+            }
+        }
+
         private void RenderHouseInterior()
         {
-
             SetupLighting();
-
             
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
-
-            
+       
             Gl.glMatrixMode(Gl.GL_PROJECTION);
             Gl.glLoadIdentity();
             Glu.gluPerspective(45.0, AnT.Width / (double)AnT.Height, 0.1, 100.0);
@@ -615,18 +667,24 @@ namespace KP_Stepanyan_PRI_121
             Gl.glEnd();
             Gl.glLineWidth(1.0f);
 
+            Gl.glPushMatrix();
+            Gl.glTranslatef(-5.0f, -4.35f + 0.5f, 1.5f); // Позиция вазы
+            Gl.glRotatef(vaseRotationAngle, 1.0f, 3.0f, 3.0f); // Вращение вазы
+            DrawRevolvedVase();
+            Gl.glPopMatrix();
+
             // Рисуем "картину" с фрактальной линией (кривая дракона)
-            float[] yellowColor = new float[] { 1.0f, 1.0f, 0.0f }; // R, G, B
+            float[] yellowColor = new float[] { 1.0f, 1.0f, 0.0f }; 
             DrawFractalDragonCurve(2.0f, 3.5f, -4.98f, 2.5f, 12, yellowColor);
 
             // Второй фрактал - зеленый
-            float[] greenColor = new float[] { 0.0f, 1.0f, 0.0f }; // R, G, B
+            float[] greenColor = new float[] { 0.0f, 1.0f, 0.0f }; 
             DrawFractalDragonCurve(-5.0f, 3.5f, -4.98f, 2.5f, 9, greenColor);
-
-
+            
             DrawLion(0.0f, -4.9f, 1.0f); // Рисуем льва внутри дома
             DrawCrane(3.0f, -2.9f, 5.0f); // Рисуем журавля (парикхмахера)
             DrawTable(-5.0f, -6.0f, 1.5f); // Рисуем стол
+
             RenderTimer.Start();
             AnT.Refresh();
         }
@@ -658,7 +716,7 @@ namespace KP_Stepanyan_PRI_121
             }
 
             // Рисуем полученную кривую с заданным цветом
-            Gl.glColor3fv(color); // Используем переданный цвет
+            Gl.glColor3fv(color);
             Gl.glBegin(Gl.GL_LINE_STRIP);
             foreach (PointF p in points)
             {
@@ -667,8 +725,8 @@ namespace KP_Stepanyan_PRI_121
             Gl.glEnd();
 
             // Увеличенная рамка для картины
-            Gl.glColor3f(0.0f, 0.0f, 0.0f); // Черный цвет
-            Gl.glLineWidth(3.0f); // Более светлая коричневая рамка
+            Gl.glColor3f(0.0f, 0.0f, 0.0f);
+            Gl.glLineWidth(3.0f); 
             float frameWidth = size * 0.7f; // Ширина рамки (20% от размера)
             float frameHeight = size * 1.0f; // Высота рамки (30% от размера)
 
@@ -728,8 +786,6 @@ namespace KP_Stepanyan_PRI_121
             Gl.glPopMatrix();
         }
 
-
-
         // Выйти из парикмахерской
         private void button3_Click(object sender, EventArgs e)
         {
@@ -757,21 +813,27 @@ namespace KP_Stepanyan_PRI_121
                 if (wingAngle < -30.0f) wingDirectionUp = true;
             }
 
+            vaseRotationAngle += vaseRotationSpeed;
+            if (vaseRotationAngle > 360.0f)
+                vaseRotationAngle -= 360.0f;
+
+
+
+
             RenderScene();
-
-
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            if (!isInsideHouse) return; // Работает только в доме
+            // Если не в парикмахерской — выходим
+            if (!isInsideHouse) return;
 
             isSmallHeadMode = !isSmallHeadMode; // Переключаем режим
 
             if (isSmallHeadMode)
             {
                 lionHeadSize = 1.2f;  // Уменьшенная голова
-                hasEars = true;       // Показываем ушки
+                hasEars = true;      // Показываем ушки
             }
             else
             {
@@ -781,5 +843,67 @@ namespace KP_Stepanyan_PRI_121
 
             RenderScene(); // Обновляем отрисовку
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Переключаем эффект резкости
+            isSharpnessEffectActive = !isSharpnessEffectActive;
+
+            if (isSharpnessEffectActive)
+            {
+                button2.BackColor = Color.LightGreen;
+                button2.Text = "Резкость: ВКЛ";
+            }
+            else
+            {
+                button2.BackColor = SystemColors.Control;
+                button2.Text = "Резкость: ВЫКЛ";
+            }
+
+            RenderScene();
+        }
+
+        
+
+
+     
+
+        private List<PointF> InterpolateLagrange(List<PointF> points, int numPoints)
+        {
+            List<PointF> result = new List<PointF>();
+
+            float minY = points[0].Y;
+            float maxY = points[points.Count - 1].Y;
+            float step = (maxY - minY) / (numPoints - 1);
+
+            for (int i = 0; i < numPoints; i++)
+            {
+                float y = minY + i * step;
+                float x = LagrangePolynomial(points, y);
+                result.Add(new PointF(x, y));
+            }
+
+            return result;
+        }
+
+        private float LagrangePolynomial(List<PointF> points, float y)
+        {
+            float result = 0.0f;
+            int n = points.Count;
+
+            for (int i = 0; i < n; i++)
+            {
+                float term = points[i].X;
+                for (int j = 0; j < n; j++)
+                {
+                    if (j != i)
+                        term *= (y - points[j].Y) / (points[i].Y - points[j].Y);
+                }
+                result += term;
+            }
+
+            return result;
+        }
+
     }
 }
